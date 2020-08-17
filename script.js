21f70fe978e967bcc3d907bec4f8462473a8fc4c
Kamshukov Ivan <48012591+IKamsh@users.noreply.github.com>
2020-08-17 13:50:22 +0300

    1st commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var btn = document.createElement("button");
+	btn.innerHTML = "remove";
+	btn.onclick = removeParent;
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(btn);
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
